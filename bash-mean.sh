706ba25a615f4ca448198448556ae46974401d9a
nine-inch-rails <yan.zou22@gmail.com>
2018-07-14 15:24:07 -0400

    use loop to guarantee word replacement

--- bash-mean.sh
+++ bash-mean.sh
@@ -3,18 +3,23 @@
 filename=$1
 repword=""
 lines=$(wc -l < $filename)
-for word in $(<$filename)
-do
-    if (( $RANDOM % $lines == 0 )); then
-        repword=${word//[$]/}
+flag=0
+while [ $flag -eq 0 ]; do
+    for word in $(<$filename)
+    do
+        if (( $RANDOM % $lines == 0 )); then
+            repword=${word//[$]/}
+        fi
+    done
+
+    echo $repword
+    torep=""
+    type "$repword" &>> /dev/null
+    if [ $? == 1 ]; then
+        flag=1
+        tempfileasdf=$(mktemp)
+        torep=${repword^^}
+        torep=${torep,}
+        sed -i 's/'$repword'/'$torep'/g' "$filename" | tee $tempfileasdf
     fi
 done
-
-torep=""
-type $repword &> /dev/null
-if [ $? == 1 ]; then
-    tEMPFILEASDF=$(mktemp)
-    torep=${repword^^}
-    torep=${torep,}
-    sed -i 's/'$repword'/'$torep'/g' "$filename" | tee $tEMPFILEASDF
-fi
